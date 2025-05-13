0af40de4210ef3d765acaae0bde80b52179b5ede
Aaron Galagarza <aaron.galagarzacarrillo@ucr.ac.cr>
2025-05-13 10:30:00 -0600

    Se agrega el objeto Car

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,40 @@
+package model;
+
+public class Car {
+    private String placa;
+    private String modelo;
+    private Double precio;
+
+    public Car() {
+    }
+
+    public Car(String placa, String modelo, Double precio) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
